3ff12408174434166762fd65fc6463d50d4bfe39
Yathunadh Krishnan <96873871+anonymousecode@users.noreply.github.com>
2025-04-21 13:33:54 +0530

    Update page.js

--- src/app/(general)/payment/create/page.js
+++ src/app/(general)/payment/create/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PageHeader from '@/components/shared/pageHeader/PageHeader'
-import InvoiceCreate from '@/components/payment/InvoiceCreate'
+// import InvoiceCreate from '@/components/payment/InvoiceCreate'
 
 const page = () => {
   return (
@@ -11,7 +11,7 @@ const page = () => {
       <div className='main-content'>
         <div className='row'>
           {/* <PaymentTable /> */}
-          <InvoiceCreate />
+          // <InvoiceCreate />
         </div>
       </div>
     </>
