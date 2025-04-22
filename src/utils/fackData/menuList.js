29e50ca9ea57b22c50d9c2c9655961eff1413ef3
anonymousecode <anonymousecode1@gmail.com>
2025-04-22 15:55:30 +0530

    fix

--- src/utils/fackData/menuList.js
+++ src/utils/fackData/menuList.js
@@ -221,155 +221,155 @@ export const menuList = [
         icon: 'feather-power',
         dropdownMenu: []
     },
-    {
-        id: 9,
-        name: "Develop",
-        path: "#",
-        icon: 'feather-code',
-        dropdownMenu: [
-            {
-                id: 1,
-                name: "login",
-                path: "/authentication/login",
-                subdropdownMenu: [
-                ]
-            },
-            {
-                id: 1,
-                name: "Lists",
-                path: "/widgets/lists",
-                subdropdownMenu: false
-            },
-            {
-                id: 2,
-                name: "Tables",
-                path: "/widgets/tables",
-                subdropdownMenu: false
-            },
-            {
-                id: 3,
-                name: "Charts",
-                path: "/widgets/charts",
-                subdropdownMenu: false
-            },
-            {
-                id: 4,
-                name: "Statistics",
-                path: "/widgets/statistics",
-                subdropdownMenu: false
-            },
-            {
-                id: 5,
-                name: "Miscellaneous",
-                path: "/widgets/miscellaneous",
-                subdropdownMenu: false
-            },
-            {
-                id: 2,
-                name: "register",
-                path: "#",
-                subdropdownMenu: [
-                    {
-                        id: 1,
-                        name: "User Registration",
-                        path: "/authentication/user-register",
-                    },
-                    {
-                        id: 2,
-                        name: "Student Registration",
-                        path: "/authentication/student-register",
-                    },
-                ]
-            },
-            {
-                id: 3,
-                name: "Error 404",
-                path: "/authentication/404",
-                subdropdownMenu: [
-                ]
-            },
-            {
-                id: 4,
-                name: "Reset Password",
-                path: "/authentication/reset",
-                subdropdownMenu: [
-                ]
-            },
-            {
-                id: 5,
-                name: "Verify OTP",
-                path: "/authentication/verify",
-                subdropdownMenu: [
-                ]
-            },
-            {
-                id: 6,
-                name: "Maintenance",
-                path: "/authentication/maintenance",
-                subdropdownMenu: [
-                ]
-            },
-            {
-                id: 1,
-                name: "Chat",
-                path: "/applications/chat",
-                subdropdownMenu: false
-            },
-            {
-                id: 2,
-                name: "Email",
-                path: "/applications/email",
-                subdropdownMenu: false
-            },
-            {
-                id: 3,
-                name: "Tasks",
-                path: "/applications/tasks",
-                subdropdownMenu: false
-            },
-            {
-                id: 4,
-                name: "Notes",
-                path: "/applications/notes",
-                subdropdownMenu: false
-            },
-            {
-                id: 5,
-                name: "Storage",
-                path: "/applications/storage",
-                subdropdownMenu: false
-            },
-            {
-                id: 6,
-                name: "Calendar",
-                path: "/applications/calendar",
-                subdropdownMenu: false
-            },
-            {
-                id: 1,
-                name: "Sales Report",
-                path: "/reports/sales",
-                subdropdownMenu: false
-            },
-            {
-                id: 2,
-                name: "Leads Report",
-                path: "/reports/leads",
-                subdropdownMenu: false
-            },
-            {
-                id: 3,
-                name: "Project Report",
-                path: "/reports/project",
-                subdropdownMenu: false
-            },
-            {
-                id: 4,
-                name: "Timesheets Report",
-                path: "/reports/timesheets",
-                subdropdownMenu: false
-            },
-        ]
-    },
+    // {
+    //     id: 9,
+    //     name: "Develop",
+    //     path: "#",
+    //     icon: 'feather-code',
+    //     dropdownMenu: [
+    //         {
+    //             id: 1,
+    //             name: "login",
+    //             path: "/authentication/login",
+    //             subdropdownMenu: [
+    //             ]
+    //         },
+    //         {
+    //             id: 1,
+    //             name: "Lists",
+    //             path: "/widgets/lists",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 2,
+    //             name: "Tables",
+    //             path: "/widgets/tables",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 3,
+    //             name: "Charts",
+    //             path: "/widgets/charts",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 4,
+    //             name: "Statistics",
+    //             path: "/widgets/statistics",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 5,
+    //             name: "Miscellaneous",
+    //             path: "/widgets/miscellaneous",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 2,
+    //             name: "register",
+    //             path: "#",
+    //             subdropdownMenu: [
+    //                 {
+    //                     id: 1,
+    //                     name: "User Registration",
+    //                     path: "/authentication/user-register",
+    //                 },
+    //                 {
+    //                     id: 2,
+    //                     name: "Student Registration",
+    //                     path: "/authentication/student-register",
+    //                 },
+    //             ]
+    //         },
+    //         {
+    //             id: 3,
+    //             name: "Error 404",
+    //             path: "/authentication/404",
+    //             subdropdownMenu: [
+    //             ]
+    //         },
+    //         {
+    //             id: 4,
+    //             name: "Reset Password",
+    //             path: "/authentication/reset",
+    //             subdropdownMenu: [
+    //             ]
+    //         },
+    //         {
+    //             id: 5,
+    //             name: "Verify OTP",
+    //             path: "/authentication/verify",
+    //             subdropdownMenu: [
+    //             ]
+    //         },
+    //         {
+    //             id: 6,
+    //             name: "Maintenance",
+    //             path: "/authentication/maintenance",
+    //             subdropdownMenu: [
+    //             ]
+    //         },
+    //         {
+    //             id: 1,
+    //             name: "Chat",
+    //             path: "/applications/chat",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 2,
+    //             name: "Email",
+    //             path: "/applications/email",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 3,
+    //             name: "Tasks",
+    //             path: "/applications/tasks",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 4,
+    //             name: "Notes",
+    //             path: "/applications/notes",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 5,
+    //             name: "Storage",
+    //             path: "/applications/storage",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 6,
+    //             name: "Calendar",
+    //             path: "/applications/calendar",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 1,
+    //             name: "Sales Report",
+    //             path: "/reports/sales",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 2,
+    //             name: "Leads Report",
+    //             path: "/reports/leads",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 3,
+    //             name: "Project Report",
+    //             path: "/reports/project",
+    //             subdropdownMenu: false
+    //         },
+    //         {
+    //             id: 4,
+    //             name: "Timesheets Report",
+    //             path: "/reports/timesheets",
+    //             subdropdownMenu: false
+    //         },
+    //     ]
+    // },
     
 ]
